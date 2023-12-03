f9cea726f16a94d487813f35578443e76b14e74f
Jacques Mendieta <jacmenhe@alumni.uv.es>
2023-12-03 01:46:36 +0100

    Header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    FooterComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
